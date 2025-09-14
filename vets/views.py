e141749af85eb010e25b86f385cdce27968e30b2
sadmanx10 <ashe6575@gmail.com>
2025-09-15 01:58:58 +0600

    Day care updated.

--- vets/views.py
+++ vets/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import VetClinic
 
 def index(request):
-    return render(request, 'vets/index.html')  # create index.html template in vets/templates/vets/
+    return render(request, "vets/index.html")
 
 def clinic_list(request):
-    return render(request, "vets/user_selection.html")  # Render the user_selection.html template
+    clinics = VetClinic.objects.all()
     return render(request, "vets/clinic_list.html", {"clinics": clinics})
